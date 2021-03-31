e5e8fa92c1a3c020890cc43fca798d50138583b4
Do Viet Duy <dovietduy97@gmail.com>
2021-03-31 15:23:14 +0700

    no message

---
+++ VTCNow/NOW/View/ViewMoreCell.swift
@@ -0,0 +1,17 @@
+//
+//  ViewMoreCell.swift
+//  NOW
+//
+//  Created by dovietduy on 3/31/21.
+//
+
+import UIKit
+
+class ViewMoreCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
